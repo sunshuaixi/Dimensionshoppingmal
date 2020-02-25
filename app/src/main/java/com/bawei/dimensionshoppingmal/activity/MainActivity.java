0a42ac5c98f11bd0701653723d2898aa94c59660
孙帅喜 <2410370911@qq.com>
2020-02-25 13:44:52 +0800

    2.20

---
+++ app/src/main/java/com/bawei/dimensionshoppingmal/activity/MainActivity.java
@@ -0,0 +1,22 @@
+package com.bawei.dimensionshoppingmal.activity;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bawei.dimensionshoppingmal.R;
+
+/**
+ * TIme:2020/2/25
+ * Author:孙帅喜
+ * Descriotion:
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
